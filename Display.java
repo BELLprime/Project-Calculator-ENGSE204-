9192005f40124b06532a4f498a35404c0db20bd1
Mongkol88 <flucktv25@gmail.com>
2026-01-31 15:14:41 +0700

    Add Display feature

--- Display.java
+++ Display.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Display {
-    
+    private double result = 0;
+
+    public Display(double initresult) {
+        this.result = initresult;
+    }
+    public void Interface() {
+        System.out.println("Enter 0-9 to Add Number");
+        System.out.println("Enter (+ - * / %) to Operator ");
+    }
+
+    public void DisplayResult() {
+        System.out.println("= " + this.result);
+    }
 }
